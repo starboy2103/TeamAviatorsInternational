6ed30e6354c66eeaddffc06056e0c3be69dba227
kowshal <kowshal2103@gmail.com>
2022-01-08 19:13:44 +0530

    Switch update

--- src/App.js
+++ src/App.js
@@ -11,14 +11,14 @@ import Teams from './components/Teams';
 import Batches from './components/Batches';
 import Project from './components/Projects';
 import Gallery from './components/Gallery';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Switch, Route, Switch} from 'react-router-dom';
 
 
 function App() {
   return (
     <>
       <Router>  {/*hashType='slash' */}
-          <Route path="/" component={Navbar}/>
+          {/*<Route path="/" component={Navbar}/>
           <Route path="/" exact component={Carousel} />
           <Route path="/" exact component={Aboutus} />
           <Route path="/" exact component={Sponsors} />
@@ -26,11 +26,26 @@ function App() {
           <Route path="/" component={RequestProject} />
           <Route path="/" component={Doubts} />
           {/* <Route path="/team" component={Navbar} element={<Navbar highlight="btn-ylw"/>}/> */}
-          <Route path="/team" component={Teams} />
+          {/*<Route path="/team" component={Teams} />
           <Route path="/team" component={Batches} />
           <Route path="/project" component={Project} />
           <Route path="/gallery" component={Gallery} />
-          <Route path="/" component={Footer} />
+          <Route path="/" component={Footer} />*/}
+          <Switch>
+            <Route path="/" component={Navbar}/>
+            <Route path="/" exact component={Carousel} />
+            <Route path="/" exact component={Aboutus} />
+            <Route path="/" exact component={Sponsors} />
+            <Route path="/" exact component={Achievement} />
+            <Route path="/" component={RequestProject} />
+            <Route path="/" component={Doubts} />
+            {/* <Route path="/team" component={Navbar} element={<Navbar highlight="btn-ylw"/>}/> */}
+            <Route path="/team" component={Teams} />
+            <Route path="/team" component={Batches} />
+            <Route path="/project" component={Project} />
+            <Route path="/gallery" component={Gallery} />
+            <Route path="/" component={Footer} />
+          </Switch>
       </Router>
     </>
   );
