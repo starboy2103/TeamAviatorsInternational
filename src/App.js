1ed4abe0008c12ca883d8a79adc01ddf51ad0f3a
kowshal <kowshal2103@gmail.com>
2022-01-08 20:19:18 +0530

    Switch update

--- src/App.js
+++ src/App.js
@@ -11,39 +11,26 @@ import Teams from './components/Teams';
 import Batches from './components/Batches';
 import Project from './components/Projects';
 import Gallery from './components/Gallery';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import {HashRouter as Router, Switch, Route} from 'react-router-dom';
 
 
 function App() {
   return (
     <>
-      <Router>  {/*hashType='slash' */}
-          {/*<Route path="/" component={Navbar}/>
-          <Route path="/" exact component={Carousel} />
-          <Route path="/" exact component={Aboutus} />
-          <Route path="/" exact component={Sponsors} />
-          <Route path="/" exact component={Achievement} />
-          <Route path="/" component={RequestProject} />
-          <Route path="/" component={Doubts} />
-          {/* <Route path="/team" component={Navbar} element={<Navbar highlight="btn-ylw"/>}/> */}
-          {/*<Route path="/team" component={Teams} />
-          <Route path="/team" component={Batches} />
-          <Route path="/project" component={Project} />
-          <Route path="/gallery" component={Gallery} />
-          <Route path="/" component={Footer} />*/}
-            <Route path="/" component={Navbar}/>
-            <Route path="/" exact component={Carousel} />
-            <Route path="/" exact component={Aboutus} />
-            <Route path="/" exact component={Sponsors} />
-            <Route path="/" exact component={Achievement} />
-            <Route path="/" component={RequestProject} />
-            <Route path="/" component={Doubts} />
-            {/* <Route path="/team" component={Navbar} element={<Navbar highlight="btn-ylw"/>}/> */}
-            <Route path="/team" exact component={Teams} />
-            <Route path="/team" exact component={Batches} />
-            <Route path="/project" exact component={Project} />
-            <Route path="/gallery" exact component={Gallery} />
-            <Route path="/" component={Footer} />
+      <Router hashType='slash'>  {/*hashType='slash' */}
+        <Route path="/" component={Navbar}/>
+        <Route path="/" exact component={Carousel} />
+        <Route path="/" exact component={Aboutus} />
+        <Route path="/" exact component={Sponsors} />
+        <Route path="/" exact component={Achievement} />
+        <Route path="/" component={RequestProject} />
+        <Route path="/" component={Doubts} />
+        {/* <Route path="/team" component={Navbar} element={<Navbar highlight="btn-ylw"/>}/> */}
+        <Route path="/team" exact component={Teams} />
+        <Route path="/team" exact component={Batches} />
+        <Route path="/project" exact component={Project} />
+        <Route path="/gallery" exact component={Gallery} />
+        <Route path="/" component={Footer} />
       </Router>
     </>
   );
