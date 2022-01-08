92012ecb93c678b0633bb1db26a584ec6012f107
kowshal <kowshal2103@gmail.com>
2022-01-08 19:52:15 +0530

    HashRouter update 9

--- src/App.js
+++ src/App.js
@@ -39,10 +39,10 @@ function App() {
             <Route path="/" component={RequestProject} />
             <Route path="/" component={Doubts} />
             {/* <Route path="/team" component={Navbar} element={<Navbar highlight="btn-ylw"/>}/> */}
-            <Route path="/team" component={Teams} />
-            <Route path="/team" component={Batches} />
-            <Route path="/project" component={Project} />
-            <Route path="/gallery" component={Gallery} />
+            <Route path="/team" exact component={Teams} />
+            <Route path="/team" exact component={Batches} />
+            <Route path="/project" exact component={Project} />
+            <Route path="/gallery" exact component={Gallery} />
             <Route path="/" component={Footer} />
       </Router>
     </>
